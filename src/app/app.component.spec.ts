d2ebd4cbcb68f3f2c7ccd465ecd25a63b5490c1a
Jose Lopez <jose.lopez@payco.co>
2021-11-03 15:13:21 -0400

    initial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'ahorcado'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ahorcado');
+    expect(app.palabra).toEqual('ahorcado');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,73 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ahorcado';
+
+  palabra ='BENJAMIN';
+  palabraOculta = '';
+  intentos = 0;
+
+  gano = false;
+  perdio = false;
+
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+            'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+
+
+
+constructor(){
+  const arr = ["BENJAMIN", "AGUACATE","CELULAR", "PERRO"];
+  this.palabra =  arr[Math.floor(Math.random() * arr.length)];
+  this.palabraOculta = '_ '.repeat(this.palabra.length);
+}
+
+    comprobar( letra: string ){
+          this.existeLetra( letra )
+          const palabraOcultaArr = this.palabraOculta.split(' ');
+
+          for (let i = 0; i < this.palabra.length; i++) {
+
+            if (this.palabra[i] === letra) {
+
+              palabraOcultaArr[i] = letra;
+
+
+            }
+          }
+          this.palabraOculta = palabraOcultaArr.join(' ')
+          this.verificarGane();
+        }
+
+        verificarGane(){
+          const palabraArr = this.palabraOculta.split(' ');
+          const palabraEvaluar = palabraArr.join('');
+
+          if (palabraEvaluar === this.palabra) {
+            this.gano= true;
+          }
+
+          if (this.intentos >= 9) {
+            this.perdio = true;
+          }
+
+        }
+
+
+
+    existeLetra( letra: string ){
+
+      if (this.palabra.indexOf(letra) >= 0 ) {
+
+      }else{
+        this.intentos ++;
+      }
+    }
+
+    recargar(){
+      location.reload();
+    }
+
 }
